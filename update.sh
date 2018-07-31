a993525f57afc28ab27e434d82f6cdd330348d3d
John Wood <john@kolon.co.uk>
2018-07-31 23:17:18 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,32 +3,32 @@
 # Get cgi script version
 if [[ -f /root/get_iplayer.cgi ]]
 then
-  VERSIONcgi = $(cat /root/get_iplayer.cgi | grep VERSION | grep -oP 'VERSION\ =\ \K.*?(?=;)' | head -1)
+  VERSIONcgi=$(cat /root/get_iplayer.cgi | grep VERSION | grep -oP 'VERSION\ =\ \K.*?(?=;)' | head -1)
 fi
 
 # Get main script version
 if [[ -f /root/get_iplayer ]]
 then
-  VERSION = $(cat /root/get_iplayer | grep version | grep -oP 'version\ =\ \K.*?(?=;)' | head -1)
+  VERSION=$(cat /root/get_iplayer | grep version | grep -oP 'version\ =\ \K.*?(?=;)' | head -1)
 fi
 
 # Get current github release version
-RELEASE = $(wget -q -O - "https://api.github.com/repos/get-iplayer/get_iplayer/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
+RELEASE=$(wget -q -O - "https://api.github.com/repos/get-iplayer/get_iplayer/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
 
 # If no github version returned
-if [[ "$RELEASE" -eq "" ]] && [[ "$FORCEDOWNLOAD" -eq "" ]]
+if [[ "$RELEASE" == "" ]] && [[ "$FORCEDOWNLOAD" -eq "" ]]
 then
   #indicates something wrong with the github call
   echo -e ******** Warning - unable to check latest release!!  Please raise an issue https://github.com/kolonuk/get_iplayer-docker/issues/new
 fi
 
-if [[ "$VERSION" -ne "$VERSIONcgi" ]] || \
-   [[ "$VERSION" -eq "" ]] || \
-   [[ "$VERSIONcgi" -eq "" ]] || \
-   [[ "$VERSION" -ne "$RELEASE" ]] || \
-   [[ "$FORCEDOWNLOAD" -ne "" ]]
+if [[ "$VERSION" != "$VERSIONcgi" ]] || \
+   [[ "$VERSION" == "" ]] || \
+   [[ "$VERSIONcgi" -== "" ]] || \
+   [[ "$VERSION" != "$RELEASE" ]] || \
+   [[ "$FORCEDOWNLOAD" != "" ]]
 then
-  if [[ "$RELEASE" -eq "" ]]
+  if [[ "$RELEASE" == "" ]]
   then
     # No release returned from github, download manually
     wget -q https://raw.githubusercontent.com/get-iplayer/get_iplayer/master/get_iplayer.cgi -O /root/get_iplayer.cgi
@@ -36,8 +36,8 @@ then
     chmod 755 /root/get_iplayer
   else
     # Download release
-    wget https://github.com/get-iplayer/get_iplayer/archive/v$RELEASE.tar.gz -O /root/latest.tar.gz
-    tar -xzf /root/latest.tar.gz --directory /root/
+    wget -q https://github.com/get-iplayer/get_iplayer/archive/v$RELEASE.tar.gz -O /root/latest.tar.gz
+    tar -xzf /root/latest.tar.gz get_iplayer-$RELEASE --directory /root/
     rm /root/latest.tar.gz
   fi
   
